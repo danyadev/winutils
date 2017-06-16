10a4a51a747283c529af5029253968f5f36119ad
evgeny <me@ch1p.com>
2017-06-17 02:51:02 +0300

    add escapeShellArg func

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 'use strict'
-module.exports = require('./build/Release/winutils.node')
+
+const bin = require('./build/Release/winutils.node')
+
+module.exports = {
+  escapeShellArg(arg) {
+    arg = arg.replace(/["%!]/g, ' ')
+    return '"' + arg + '"'
+  },
+
+  deelevate: bin.deelevate,
+  elevate: bin.elevate,
+  getSystem32Path: bin.getSystem32Path,
+  isUserAdmin: bin.isUserAdmin
+}
